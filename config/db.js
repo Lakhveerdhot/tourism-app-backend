0c860a38ecf7656d501fabc698703b978d485965
Lakhveer Singh <lakhveerdhot120@gmail.com>
2026-03-19 11:23:41 +0530

    fix db connection

--- config/db.js
+++ config/db.js
@@ -1,11 +1,26 @@
+// const { Pool } = require('pg');
+
+// const pool = new Pool({
+//   user: 'postgres',
+//   host: 'localhost',
+//   database: 'tourism_db',
+//   password: 'lakhu123#',
+//   port: 5433,
+// });
+
+// module.exports = pool;
+
+
+
+
+
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'tourism_db',
-  password: 'lakhu123#',
-  port: 5433,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
